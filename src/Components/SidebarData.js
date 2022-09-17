7d97585fbd560583c9e9c85ecbbd8b5ffdd0a071
durga durga <kalyanidurga292@gmail.com>
2022-09-17 10:35:47 +0530

    firstcommit

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import InsertChartRoundedIcon from '@mui/icons-material/InsertChartRounded';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import UpdateIcon from '@mui/icons-material/Update';
+
+export const  SidebarDate =[
+    {
+        title:"Insert",
+        icon:< InsertChartRoundedIcon/>,
+        link:"/InsertChartRounded"
+    },
+    {
+        title:"Preview",
+        icon:<  VisibilityIcon/>,
+        link:"/Visibility"
+    },
+    {
+        title:"Update",
+        icon:<UpdateIcon/>,
+        link:"/Update"
+    }
+]
+
